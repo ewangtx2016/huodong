b88b93008c38f529637bbc7da5c9326c4a39f08c
ewangtx2016 <18664698@qq.com>
2017-09-15 11:08:04 +0800

    公共样式

---
+++ src/public/js/public.js
@@ -0,0 +1,14 @@
+$(function(){
+    var clenWidth = function(){
+      var bodyWidth = $(window).width();
+      var newHhtmlFontSzie = 100 * (bodyWidth/750) + 'px';
+      $('html').css({
+        'font-size'  :   newHhtmlFontSzie
+      })
+    }
+    clenWidth();
+
+    window.onresize = function(){
+      clenWidth();
+    }
+})
