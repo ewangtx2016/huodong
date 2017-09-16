4e26d227a9075b5bdbff538435e52b12059e501d
ewangtx2016 <18664698@qq.com>
2017-09-17 03:09:44 +0800

    凌晨的页面提交

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,35 @@
 <template>
-    <div>
-      登录
+    <div class="lv_login">
+    	<div class="lv_close" @touchstart="hide"></div>
+    	<div class="lv_logbox">
+    		<p>输入手机号，参与拼团机会。</p>
+    		<div class="lv_li lv_li_tel">
+    			<input type="tel" class="lv_input" placeholder="请填写手机号" />
+    			<a href="javascript:;" class="lv_but_tl">获取<br />验证码</a>
+    		</div>
+    		<div class="lv_li">
+    			<input type="number" class="lv_input" placeholder="填写验证码" />
+    		</div>
+    		<div class="lv_sub">
+    			<img src="../public/img/lv_but1.png" />
+    		</div>
+    	</div>
     </div>
 </template>
 
 <script>
     export default {
         data() {
-            return {}
+            return {
+            	close	:	false
+            }
         },
         components: {},
-        methods: {}
+        methods: {
+        	hide:function(){
+        		this.$emit('change-v',this.close);
+        	}
+        }
     }
 </script>
 
--- src/components/Sign.vue
+++ src/components/Sign.vue
@@ -1,7 +1,26 @@
 <template>
-    <div>
-     注册
-    </div>
+   <div class="lv_sign">
+   		<div class="sig_box">
+   			<p>
+   				<input class="lv_inp" placeholder="姓名" type="text" name="" id="" value="" />
+   			</p>
+   			<p>
+   				<input class="lv_inp" placeholder="电话" type="tel" name="" id="" value="" />
+   			</p>
+   			<p>
+   				<input class="lv_inp" placeholder="所在高校" type="text" name="" id="" value="" />
+   			</p>
+   			<p>
+   				<input class="lv_inp" placeholder="所学专业" type="text" name="" id="" value="" />
+   			</p>
+   			
+   			
+
+   			<div @touchstart="submit" class="lv_tijiao" href="javascript:;"><img src="../public/img/lv_tijiao.png" /></div>
+
+   		
+   		</div>
+   </div>
 </template>
 
 <script>
@@ -10,7 +29,16 @@
             return {}
         },
         components: {},
-        methods: {}
+        methods: {
+        	submit:function(){
+        		if(confirm('确定要提交吗？')){
+        			alert('提交成功！');
+        			this.$router.push({
+        				path:'/'
+        			})
+        		}
+        	}
+        }
     }
 </script>
 
